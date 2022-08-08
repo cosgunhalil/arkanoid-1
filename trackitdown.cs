8164d475d1c5b453fd6ef5cee9f90fcc7c3c5b91
TurkovaEnhar <110021017+TurkovaEnhar@users.noreply.github.com>
2022-08-08 16:53:58 +0300

    Add files via upload

--- trackitdown.cs
+++ trackitdown.cs
@@ -8,7 +8,7 @@ public class trackitdown : MonoBehaviour
      [SerializeField] GameObject target;
       [SerializeField] GameObject eyeball;
      void Update()
-     {
+     {    ///png fotoyu targetın x'i ile fotonun x'inin farkı kadar döndürüyorum.
          transform.rotation= Quaternion.Euler(0,0,(eyeball.transform.position.x*5-target.transform.position.x*5));
      }
 }
