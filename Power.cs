a5bcceae2ce3a15c77039b5501db2b19c438f974
Halil Coşgun <cosgun.halil@gmail.com>
2022-08-11 10:41:18 +0300

    chore(suggestion): Add suggestions as TODO

--- Power.cs
+++ Power.cs
@@ -19,16 +19,18 @@ public class Power : MonoBehaviour
      } 
     }
     void positionsOfTheExtraBalls(){
+    //TODO: use for or foreach!!!
       spawnTheBall[0].transform.position=new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
       spawnTheBall[1].transform.position=new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
       spawnTheBall[2].transform.position=new Vector3(this.transform.position.x,this.transform.position.y,this.transform.position.z);
     }
     //3 tane ball objesini topun çarptığı objenin konumunda üret ve topun çarptığı objeyi yok et.
     void Spawnball(){
+    //TODO: use for or foreach!!!
       spawnTheBall[0]= Object.Instantiate(ExtraBall[0]) as GameObject;
       spawnTheBall[1]= Object.Instantiate(ExtraBall[1]) as GameObject;
       spawnTheBall[2]= Object.Instantiate(ExtraBall[2]) as GameObject;
       positionsOfTheExtraBalls();
-      Destroy(this.gameObject);
+      Destroy(this.gameObject); //TODO: use object pool design pattern!!!
        }
 }
